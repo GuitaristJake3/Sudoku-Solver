cf0a311bd6f6ac8f940859f818c06c79808458ce
GuitaristJake3 <47261035+GuitaristJake3@users.noreply.github.com>
2021-11-10 04:18:37 +0000

    Added methods to make any selected space yellow.

--- Sudoku Solver/Sudoku.cs
+++ Sudoku Solver/Sudoku.cs
@@ -12,9 +12,20 @@ namespace Sudoku_Solver
 {
     public partial class SudokuSolver : Form
     {
+        List<NumericUpDown> r2;
+        List<NumericUpDown> r3;
+        List<NumericUpDown> r4;
+        List<NumericUpDown> r5;
+        List<NumericUpDown> r6;
+        List<NumericUpDown> r7;
+        List<NumericUpDown> r8;
+        List<NumericUpDown> r9;
         public SudokuSolver()
         {
             InitializeComponent();
+            List<NumericUpDown> r1 = new List<NumericUpDown> {
+                r1c1, r1c2, r1c3, r1c4, r1c5, r1c6, r1c7, r1c8, r1c9
+            }; 
         }
 
         private void Space_Enter(object sender, EventArgs e)
