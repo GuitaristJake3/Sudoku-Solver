90508ccb5ee55317d3478dd63962e39256c044c8
GuitaristJake3 <47261035+GuitaristJake3@users.noreply.github.com>
2021-11-11 18:22:08 +0000

    Made class for rows, columns and squares.

---
+++ Sudoku Solver/RCS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_Solver
+{
+    class RCS
+    {
+        decimal[] spaces;
+        public RCS(decimal one, decimal two, decimal three, decimal four, decimal five, decimal six, decimal seven, decimal eight, decimal nine)
+        {
+            spaces = new decimal[] {one, two, three, four, five, six, seven, eight, nine};
+        }
+
+        public decimal[] Spaces
+        {
+            get { return spaces; }
+            set { spaces = value; }
+        }
+
+    }
+}
--- Sudoku Solver/Sudoku.cs
+++ Sudoku Solver/Sudoku.cs
@@ -12,9 +12,17 @@ namespace Sudoku_Solver
 {
     public partial class SudokuSolver : Form
     {
+        RCS row1, row2, row3, row4, row5, row6, row7, row8, row9;
         public SudokuSolver()
         {
             InitializeComponent();
+            row1 = new RCS(r1c1.Value, r1c2.Value, r1c3.Value, r1c4.Value, r1c5.Value, r1c6.Value, r1c7.Value, r1c8.Value, r1c9.Value);
+            row2 = new RCS(r2c1.Value, r2c2.Value, r2c3.Value, r2c4.Value, r2c5.Value, r2c6.Value, r2c7.Value, r2c8.Value, r2c9.Value);
+        }
+
+        private void Update_Grid()
+        {
+            row1.Spaces = { r1c1.Value, r1c2.Value, r1c3.Value, r1c4.Value, r1c5.Value, r1c6.Value, r1c7.Value, r1c8.Value, r1c9.Value};
         }
 
         private void Space_Enter(object sender, EventArgs e)    //Each space turns yellow when control gained
